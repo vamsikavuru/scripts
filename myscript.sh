c0e0acd95fb0a1a03cc91f284704ca1fbf126bca
sunil <kvk@gmail.com>
2017-09-11 16:14:10 +0530

    shell script

---
+++ myscript.sh
@@ -0,0 +1,39 @@
+!# /bin/bash
+###########################
+
+# purpose : write basicscripts
+
+# owner : info@info.com
+
+# version : 1.0
+
+# Input : none 
+
+# output : none
+
+#########################
+
+#clear the screen 
+clear
+
+ echo "welcome devops"
+ 
+sleep 5 "Shell Practice" 
+
+"ended"!# /bin/bash
+###########################
+
+# purpose : write basicscripts
+
+# owner : info@info.com
+
+# version : 1.0
+
+# Input : none
+
+# output : none
+
+#########################
+ 
+
+
