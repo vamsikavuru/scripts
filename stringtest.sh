1dc447b3e82be07213ccd5fbb0b0908041366333
vamsi <kvk@info.com>
2017-09-13 15:16:50 +0530

    updated

--- stringtest.sh
+++ stringtest.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
- 
+#####################################
+# Purpose : To work with strings
+# Owner   : vamsi@nareshit.com
+# version : 1.0
+# Input   : None
+# output  : None
+#####################################
+# check user wheather devops or not
+WHOAMI=`/usr/bin/whoami`
 
+ if [ "$WHOAMI" == "devops" ]; then
+   echo "you are $WHOAMI user "
+ else
+    echo "you are not dev user, you are another"
+ fi
 
-VARNAME=shell script
-
-echo '$VARNAME'
-
-echo "$VARNAME"
-
-echo `$VARNAME`
-
+  
 
